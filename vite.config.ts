6d45091c21000bf827d6cdf1c5a043f3d59c306c
zidane-arkan <zidark897@Gmail.com>
2023-04-07 13:19:00 +0700

    Add PWA Function

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Perbakin PWA",
+    short_name: "Perbakin",
+    description: "Aplikasi untuk Persatuan Tembak Indonesia",
+    icons: [
+      {
+        src: "android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#1976d2",
+    background_color: "#fafafa",
+    display: "standalone",
+    scope: "./",
+    start_url: "./",
+    orientation: "portrait",
+  },
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
