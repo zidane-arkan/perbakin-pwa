5905fd59934c6e9559c2864dde1d1be4c3047d53
zidane-arkan <zidark897@Gmail.com>
2023-05-11 16:04:16 +0700

    Update hasil ujian V1.5, Add icon in for sort

--- src/layout/admin/HasilUjian.tsx
+++ src/layout/admin/HasilUjian.tsx
@@ -10,12 +10,7 @@ import {
 } from '@tanstack/react-table';
 import styled from 'styled-components';
 // Gambar
-// import avatar from '../../../app-assets/avatar.png';
-import targetWhite from '../../../app-assets/targetwhite.png'
-import mapPin from '../../../app-assets/map_pin.png'
-import clock from '../../../app-assets/clock.png'
-import peluruBulat from '../../../app-assets/pelurubulat.png';
-import tambahPenguji from '../../../app-assets/tambahpenguji.png'
+import arrowdown from '../../app-assets/arrowdown.png'
 // react router dom
 import { Link } from 'react-router-dom';
 
@@ -161,15 +156,18 @@ const HasilUjian = () => {
         <Layout className={'rounded-3xl gap-8 mt-28 pt-[2%] overflow-hidden'}>
             <HeaderWhiteCustom typeIcon='returnblack' title='Hasil Ujian' />
             <LayoutChild className='flex-col gap-0 h-auto'>
-                <form className='flex flex-col w-full h-auto justify-between'>
-                    <div className="relative">
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <form className='flex w-full h-auto items-center justify-between'>
+                    <div className="w-[80%] relative">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 ">
                             <svg className="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
                                 <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#1B79B8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                             </svg>
                         </span>
-                        <input type="text" className="w-full py-3 pl-10 pr-4 text-gray-700 bg-white border rounded-full focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" placeholder="Cari" />
+                        <input type="text" className="w-full py-3 pl-10 pr-4 shadow-md text-gray-700 bg-white border rounded-full focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" placeholder="Cari" />
                     </div>
+                    <button className="flex items-center justify-center w-[15%] h-full rounded-full bg-[#1B79B8]">
+                        <img src={arrowdown} />
+                    </button>
                 </form>
                 <TabelHasilUjian />
                 <section></section>
