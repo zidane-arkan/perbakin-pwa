4a6cd8d159ea920ff1cda6785cb08b3159032a96
zidane-arkan <zidark897@Gmail.com>
2023-05-11 16:03:50 +0700

    Update hasil ujian V1.4, Add form search

--- src/layout/admin/HasilUjian.tsx
+++ src/layout/admin/HasilUjian.tsx
@@ -162,8 +162,13 @@ const HasilUjian = () => {
             <HeaderWhiteCustom typeIcon='returnblack' title='Hasil Ujian' />
             <LayoutChild className='flex-col gap-0 h-auto'>
                 <form className='flex flex-col w-full h-auto justify-between'>
-                    <div className="mb-6">
-                        <input type="password" id="passlama" className="bg-gray-50 border-2 border-gray-300 text-gray-700 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                    <div className="relative">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <svg className="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
+                                <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#1B79B8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                        </span>
+                        <input type="text" className="w-full py-3 pl-10 pr-4 text-gray-700 bg-white border rounded-full focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" placeholder="Cari" />
                     </div>
                 </form>
                 <TabelHasilUjian />
