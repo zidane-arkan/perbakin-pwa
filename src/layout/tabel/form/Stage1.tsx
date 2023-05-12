05ea7785c878c0189be51497099a711cb0b8f094
zidane-arkan <zidark897@Gmail.com>
2023-05-12 18:28:33 +0700

    Update form for stage 1 V1.3

--- src/layout/tabel/form/Stage1.tsx
+++ src/layout/tabel/form/Stage1.tsx
@@ -125,8 +125,8 @@ const EditableCell = ({ getValue, row, column, table }: any) => {
     />
   ) : (
     <input className='w-[100px] text-center'
-        min='00:00'
-        max='00:55'
+      min='00:00'
+      max='00:55'
       type={columnMeta?.type || "text"}
       value={value}
       onChange={(e) => setValue(e.target.value)}
@@ -135,87 +135,169 @@ const EditableCell = ({ getValue, row, column, table }: any) => {
   );
 }
 
-const columns = [
+const columns1 = [
   columnHelper.group({
-    id: 'seri',
-    header: () => <span>Seri</span>,
-    // footer: props => props.column.id,
+    header: 'Percobaan 1',
     columns: [
-      columnHelper.accessor('seri', {
+      columnHelper.group({
         id: 'seri',
-        header: () => <span>No Seri</span>,
+        header: () => <span>Seri</span>,
+        // footer: props => props.column.id,
+        columns: [
+          columnHelper.accessor('seri', {
+            id: 'seri',
+            header: () => <span>No Seri</span>,
+          }),
+        ]
       }),
-    ]
-  }),
-  columnHelper.group({
-    id: 'nilaiPerkenaan',
-    header: () => <span>Nilai Perkenaan</span>,
-    // footer: props => props.column.id,
-    columns: [
-      columnHelper.accessor('A', {
-        id: 'A',
-        header: () => <span>A</span>,
-        cell: EditableCell,
-        meta: {
-          type: 'number',
-        }
+      columnHelper.group({
+        id: 'nilaiPerkenaan',
+        header: () => <span>Nilai Perkenaan</span>,
+        // footer: props => props.column.id,
+        columns: [
+          columnHelper.accessor('A', {
+            id: 'A',
+            header: () => <span>A</span>,
+            cell: EditableCell,
+            meta: {
+              type: 'number',
+            }
+          }),
+          columnHelper.accessor('B', {
+            id: 'B',
+            header: () => <span>B</span>,
+            cell: EditableCell,
+            meta: {
+              type: 'number',
+            }
+          }),
+          columnHelper.accessor('C', {
+            id: 'C',
+            header: () => <span>C</span>,
+            cell: EditableCell,
+            meta: {
+              type: 'number',
+            }
+          }),
+        ],
       }),
-      columnHelper.accessor('B', {
-        id: 'B',
-        header: () => <span>B</span>,
-        cell: EditableCell,
-        meta: {
-          type: 'number',
-        }
-      }),
-      columnHelper.accessor('C', {
-        id: 'C',
-        header: () => <span>C</span>,
-        cell: EditableCell,
-        meta: {
-          type: 'number',
-        }
-      }),
-    ],
-  }),
-  columnHelper.group({
-    id: 'waktu',
-    header: () => <span>Waktu</span>,
-    // footer: props => props.column.id,
-    columns: [
-      columnHelper.accessor('waktu', {
+      columnHelper.group({
         id: 'waktu',
-        header: () => <span>Hasil Waktu</span>,
-        cell: EditableCell,
-        meta: {
-          type: 'time',
-        }
+        header: () => <span>Waktu</span>,
+        // footer: props => props.column.id,
+        columns: [
+          columnHelper.accessor('waktu', {
+            id: 'waktu',
+            header: () => <span>Hasil Waktu</span>,
+            cell: EditableCell,
+            meta: {
+              type: 'time',
+            }
+          }),
+        ]
       }),
-    ]
-  }),
-  columnHelper.group({
-    id: 'hasil',
-    header: () => <span>Hasil</span>,
-    // footer: props => props.column.id,
-    columns: [
-      columnHelper.accessor('hasil', {
+      columnHelper.group({
         id: 'hasil',
-        header: () => <span>Hasil (Centang)</span>,
-        cell: EditableCell,
-        meta: {
-          type: 'checkbox',
-        }
+        header: () => <span>Hasil</span>,
+        // footer: props => props.column.id,
+        columns: [
+          columnHelper.accessor('hasil', {
+            id: 'hasil',
+            header: () => <span>Hasil (Centang)</span>,
+            cell: EditableCell,
+            meta: {
+              type: 'checkbox',
+            }
+          }),
+        ]
       }),
     ]
-  }),
+  })
 ]
-
-
-const Stage1 = () => {
+const columns2 = [
+  columnHelper.group({
+    header: 'Percobaan 2',
+    columns: [
+      columnHelper.group({
+        id: 'seri',
+        header: () => <span>Seri</span>,
+        // footer: props => props.column.id,
+        columns: [
+          columnHelper.accessor('seri', {
+            id: 'seri',
+            header: () => <span>No Seri</span>,
+          }),
+        ]
+      }),
+      columnHelper.group({
+        id: 'nilaiPerkenaan',
+        header: () => <span>Nilai Perkenaan</span>,
+        // footer: props => props.column.id,
+        columns: [
+          columnHelper.accessor('A', {
+            id: 'A',
+            header: () => <span>A</span>,
+            cell: EditableCell,
+            meta: {
+              type: 'number',
+            }
+          }),
+          columnHelper.accessor('B', {
+            id: 'B',
+            header: () => <span>B</span>,
+            cell: EditableCell,
+            meta: {
+              type: 'number',
+            }
+          }),
+          columnHelper.accessor('C', {
+            id: 'C',
+            header: () => <span>C</span>,
+            cell: EditableCell,
+            meta: {
+              type: 'number',
+            }
+          }),
+        ],
+      }),
+      columnHelper.group({
+        id: 'waktu',
+        header: () => <span>Waktu</span>,
+        // footer: props => props.column.id,
+        columns: [
+          columnHelper.accessor('waktu', {
+            id: 'waktu',
+            header: () => <span>Hasil Waktu</span>,
+            cell: EditableCell,
+            meta: {
+              type: 'time',
+            }
+          }),
+        ]
+      }),
+      columnHelper.group({
+        id: 'hasil',
+        header: () => <span>Hasil</span>,
+        // footer: props => props.column.id,
+        columns: [
+          columnHelper.accessor('hasil', {
+            id: 'hasil',
+            header: () => <span>Hasil (Centang)</span>,
+            cell: EditableCell,
+            meta: {
+              type: 'checkbox',
+            }
+          }),
+        ]
+      }),
+    ]
+  })
+]
+const Table1 = () => {
   const [data, setData] = useState(() => [...defaultData])
   const table = useReactTable({
     data,
-    columns,
+    columns: columns1,
     getCoreRowModel: getCoreRowModel(),
     meta: {
       updateData: ({ rowIndex, columnId, value }: any) => {
@@ -265,6 +347,93 @@ const Stage1 = () => {
         </tbody>
       </table>
     </Styles>
+  )
+}
+const Table2 = () => {
+  const [data, setData] = useState(() => [...defaultData])
+  const table = useReactTable({
+    data,
+    columns: columns2,
+    getCoreRowModel: getCoreRowModel(),
+    meta: {
+      updateData: ({ rowIndex, columnId, value }: any) => {
+        setData((old) =>
+          old.map((row, index) => {
+            if (index === rowIndex) {
+              return {
+                ...old[rowIndex],
+                [columnId]: value,
+              };
+            }
+            return row;
+          })
+        );
+      },
+    },
+  });
+  return (
+    <Styles>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id} colSpan={header.colSpan}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Styles>
+  )
+}
+const Stage1 = () => {
+  const [data, setData] = useState(() => [...defaultData])
+  const table = useReactTable({
+    data,
+    columns: columns1,
+    getCoreRowModel: getCoreRowModel(),
+    meta: {
+      updateData: ({ rowIndex, columnId, value }: any) => {
+        setData((old) =>
+          old.map((row, index) => {
+            if (index === rowIndex) {
+              return {
+                ...old[rowIndex],
+                [columnId]: value,
+              };
+            }
+            return row;
+          })
+        );
+      },
+    },
+  });
+  return (
+    <>
+      <section>
+        <Table1 />
+        <Table2 />
+      </section>
+    </>
   )
 }
 
