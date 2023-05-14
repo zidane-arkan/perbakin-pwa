06d052929280fae7da16bd080d9dfc2e891a3b47
zidane-arkan <zidark897@Gmail.com>
2023-05-14 09:54:01 +0700

    Add new file

---
+++ src/layout/penguji/ketentuan/Stage1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Form from '../../pages/Form'
+const Stage1 = () => {
+    return (
+        <Form ujian='stage1' title='Stage #1' link='/penguji/stage1_form' />
+    )
+}
+
+export default Stage1
