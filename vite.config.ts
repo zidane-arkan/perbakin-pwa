8b1ba8019820d0e25c97f7f842bd1804eb31c524
FadhilAF <falfatih3421@gmail.com>
2023-05-31 14:56:08 +0700

    remove https package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
-import mkcert from "vite-plugin-mkcert";
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType: "prompt",
@@ -38,5 +37,5 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
 };
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugin), mkcert()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 });
