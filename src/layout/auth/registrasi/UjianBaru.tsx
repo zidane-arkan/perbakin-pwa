2ee2c9ba802f8cbecdce80a53d2eb2a379128b67
zidane-arkan <zidark897@Gmail.com>
2023-06-02 21:27:55 +0700

    Update ujian baru v1.3

--- src/layout/auth/registrasi/UjianBaru.tsx
+++ src/layout/auth/registrasi/UjianBaru.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Layout, LayoutChild } from '../../../components/Layout'
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../context/AuthContext";
+import { HandlerResponse } from "../../../context/response";
 import { Link } from 'react-router-dom'
 import calenderExtraSmall from '../../../app-assets/calender_extrasmall.png';
+
+interface CreateExamElements extends HTMLFormControlsCollection {
+    name: HTMLInputElement;
+    location: HTMLInputElement;
+    organizer: HTMLInputElement;
+    begin: HTMLInputElement;
+    finish: HTMLInputElement;
+}
+
 const UjianBaru = () => {
+    const examContext = useContext(AuthContext)
+    const navigate = useNavigate();
     return (
         <Layout className={'rounded-3xl mt-20 mb-[5%]'}>
             <LayoutChild className='flex-col'>
@@ -18,7 +32,7 @@ const UjianBaru = () => {
                         </div>
                         <div className="mb-6">
                             <label htmlFor="lokasi" className="block mb-2 text-sm font-bold text-gray-900">Lokasi</label>
-                            <input name='lokasi' type="text" id="lokasi" className="bg-gray-50 border-2 border-gray-300 text-gray-700 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"  required />
+                            <input name='lokasi' type="text" id="lokasi" className="bg-gray-50 border-2 border-gray-300 text-gray-700 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
                         </div>
                         <div className="mb-6">
                             <label htmlFor="organizer" className="block mb-2 text-sm font-bold text-gray-900">Organizer</label>
@@ -35,7 +49,7 @@ const UjianBaru = () => {
                                 <input type="date" id="date-akhir" className="bg-gray-50 border-2 border-gray-300 text-gray-700 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
                             </div>
                         </div>
-                       
+
                     </section>
                     <div className='flex flex-col gap-4'>
                         <Link to={'/superadmin/adminregis'} type="submit" className="text-white rounded-lg text-base font-bold w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Selanjutnya</Link>
