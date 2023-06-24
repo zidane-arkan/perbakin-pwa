b275270f690074612125fa501c17e2926bf0ec7f
zidane-arkan <zidark897@Gmail.com>
2023-06-24 10:57:45 +0700

    Add penembak list from database

--- src/layout/pages/Penembak.tsx
+++ src/layout/pages/Penembak.tsx
@@ -6,6 +6,9 @@ type Props = {
     classname?: string | any;
     statusAuth?: boolean;
 }
+interface PenembakAdminProps {
+    shooters: string[];
+}
 export const Penembak = (props: Props) => {
     const classname = `${props.classname} rounded-3xl`;
     return (
@@ -30,8 +33,9 @@ export const Penembak = (props: Props) => {
         </>
     )
 }
-export const PenembakAdmin = (props: Props) => {
+export const PenembakAdmin: React.FC<PenembakAdminProps> = (props : any) => {
     const classname = `${props.classname} rounded-3xl`;
+    console.log(props.shooters)
     return (
         <>
             <Layout className={classname}>
@@ -39,13 +43,17 @@ export const PenembakAdmin = (props: Props) => {
                     <span className='inline text-left'>
                         <h3 className='text-lg font-bold'>List Penembak</h3>
                     </span>
-                    <CardPenembakAdmin penembak="Testing 1" klub="Asal Klub 1" stage={'Stage #2'} pengprov={'Pengprov 1'} penguji={'Penguji 1'} />
-                    <CardPenembakAdmin penembak="Testing 2" klub="Asal Klub 2" stage={'Gagal'} pengprov={'Pengprov 1'} penguji={'Penguji 1'} />
-                    <CardPenembakAdmin penembak="Testing 3" klub="Asal Klub 3" stage={'Ujian Klasifikasi 20 Meter'} pengprov={'Pengprov 1'} penguji={'Penguji 1'} />
-                    <CardPenembakAdmin penembak="Testing 4" klub="Asal Klub 4" stage={'Stage #4'} pengprov={'Pengprov 1'} penguji={'Penguji 1'} />
-                    <CardPenembakAdmin penembak="Testing 5" klub="Asal Klub 5" stage={'Stage #5'} pengprov={'Pengprov 1'} penguji={'Penguji 1'} />
-                    <CardPenembakAdmin penembak="Testing 6" klub="Asal Klub 6" stage={'Stage #6'} pengprov={'Pengprov 1'} penguji={'Penguji 1'} />
-                    <CardPenembakAdmin penembak="Testing 7" klub="Asal Klub 6" stage={'Stage #6'} pengprov={'Pengprov 1'} penguji={'Penguji 1'} />
+                    {/* <CardPenembakAdmin penembak="Testing 1" klub="Asal Klub 1" stage={'Stage #2'} pengprov={'Pengprov 1'} penguji={'Penguji 1'} /> */}
+                    {props.shooters.map((shooter : any, index) => (
+                        <CardPenembakAdmin
+                            key={index}
+                            penembak={shooter.name}
+                            klub={shooter.club}
+                            stage={'Stage #2'}
+                            pengprov={shooter.province}
+                            penguji={shooter.scorer}
+                        />
+                    ))}
                     {props.statusAuth && <Link className='flex pl-1 bg-white shadow-md pr-16 py-2 rounded-full' to='/admin/admindashboard/tambahpenembak'>
                         <span className='font-xl'>Tambah Penembak</span>
                     </Link>}
--- src/layout/super-admin/tabs/Penembak.tsx
+++ src/layout/super-admin/tabs/Penembak.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { PenembakAdmin } from '../../pages/Penembak'
+import api from '../../../api/api'
+import { AxiosError } from 'axios'
+import { ResponseData } from '../../../context/response'
+
 const Penembak = () => {
+    const [shooters, setShooters] = useState<string[]>([]);
+    const getExamId = async (): Promise<string | null> => {
+        try {
+            const response = await api.get("/super/exam");
+            const exams = response.data.data.exams;
+            if (exams.length > 0) {
+                const lastExam = exams[exams.length - 1];
+                const lastExamId = lastExam.id;
+
+                return lastExamId;
+            } else {
+                return null;
+            }
+        } catch (error) {
+            const err = error as AxiosError<ResponseData<null>>;
+            console.error("Error:", err);
+
+            return null;
+        }
+    };
+    useEffect(() => {
+        const fetchShooters = async () => {
+            try {
+                const examId = await getExamId();
+                const response = await api.get(`/super/exam/${examId}/shooter`);
+                const shooters = response.data.data.shooters;
+                setShooters(shooters);
+            } catch (error) {
+                const err = error as AxiosError<ResponseData<null>>;
+                console.error("Error:", err);
+            }
+        };
+
+        fetchShooters();
+    }, []);
+
     return (
-        <PenembakAdmin />
+        <PenembakAdmin shooters={shooters} />
     )
 }
 
