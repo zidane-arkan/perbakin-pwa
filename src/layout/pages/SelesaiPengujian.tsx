5cb493211726a7827cd376a41138cab51bd3cc8c
zidane-arkan <zidark897@Gmail.com>
2023-07-13 19:22:24 +0700

    Update Selesai Pengujian

--- src/layout/pages/SelesaiPengujian.tsx
+++ src/layout/pages/SelesaiPengujian.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import TandaTangan from './TandaTangan'
-import user1 from '../../app-assets/userbig1.png';
-import { LayoutChild } from '../../components/Layout';
+import { HeaderBlueCustom } from '../../components/Header'
+import { Layout, LayoutChild } from '../../components/Layout'
+import { CardText } from '../../components/ui/Card';
+import SignatureCanvas from 'react-signature-canvas';
+import { Link } from 'react-router-dom';
+import user1 from "../../app-assets/userbig1.png";
 
 const SelesaiPengujian = (props: any) => {
     return (
-        <TandaTangan title='Selesai Pengujian' link={'/penguji/'}>
+        <Layout className={'rounded-3xl gap-8 mt-28 pb-8 pt-[10%]'}>
+            <HeaderBlueCustom typeIcon='close' title="Selesai Pengujian" />
             <LayoutChild>
                 <section className='w-full flex flex-row items-center justify-between gap-4'>
                     <div className='flex flex-col gap-2'>
@@ -30,7 +34,28 @@ const SelesaiPengujian = (props: any) => {
                     </div>
                 </section>
             </LayoutChild>
-        </TandaTangan>
+            <LayoutChild className='justify-between gap-6'>
+                <button className='py-2 h-[48px] w-[165px] rounded-xl bg-[#62DE5F]'>Berhasil</button>
+                <button className='button-gagal py-2 rounded-xl h-[48px] w-[165px] bg-[]'>Gagal</button>
+            </LayoutChild>
+            <LayoutChild className='flex-col gap-12'>
+                <section className='flex flex-col gap-2'>
+                    <h2>Penguji:</h2>
+                    <p className='text-[#000]/60'>Isi kolom berikut dengan tanda tangan.</p>
+                    <SignatureCanvas penColor='green'
+                        canvasProps={{ width: 800, height: 400, className: 'border-gray-300 rounded-xl border-2' }} />
+                </section>
+                <section className='flex flex-col gap-2'>
+                    <h2>Peserta:</h2>
+                    <p className='text-[#000]/60'>Isi kolom berikut dengan tanda tangan.</p>
+                    <SignatureCanvas penColor='green'
+                        canvasProps={{ width: 800, height: 400, className: 'border-gray-300 rounded-xl border-2' }} />
+                </section>
+                <CardText>
+                    <Link to={'/penguji/'} className='w-full text-center px-4 py-4 text-white bg-[#036BB0] rounded-lg' type='button'>Selesai Pengujian</Link>
+                </CardText>
+            </LayoutChild>
+        </Layout>
     )
 }
 
