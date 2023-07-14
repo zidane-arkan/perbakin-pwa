ca2cade326daddb5c0e0bf9a6e4db97b36a40381
zidane-arkan <zidark897@Gmail.com>
2023-07-14 16:31:11 +0700

    Update aturan for Stage 4

--- src/layout/penguji/aturan/Stage4.tsx
+++ src/layout/penguji/aturan/Stage4.tsx
@@ -4,12 +4,20 @@ const Stage4 = () => {
     return (
         <Aturan
             title="Stage #4"
-            jarak="7"
-            waktu="6"
-            sasaran="1"
-            tembakMaks='18'
+            jarak="10"
+            waktu="15"
+            sasaran="2"
+            tembakMaks='12'
             link='stage4_form'
-        />
+            posisiAwal='Berdiri rileks menghadap sasaran, kedua tangan disamping.'
+        >
+            <li>Cabut senjata dan tembak 2 kali pada sasaran Pertama dengan tangan kuat (strong hand), pindahkan ke tangan lemah (weak hand) dan tembakan dua kali pada
+                sasaran berikutnya.</li>
+            <li>Lakukan sebanyak 3 kali.</li>
+            <li>Tembakan yang tidak mengenai sasaran (MISS) berarti gagal dalam seri tersebut.</li>
+            <li>Pelanggaran keamanan/safety akan <b>didiskualifikasi</b> dan dinyatakan <b>TIDAK LULUS</b> dari Ujian Sertifikasi dan tidak dapat mengulang pada ujian kali ini.</li>
+        </Aturan>
+
     )
 }
 
