734fdaf1a7aa1353622014a3fdf4f8ce3107a2e7
zidane-arkan <zidark897@Gmail.com>
2023-07-14 16:49:14 +0700

    Update aturan for Stage 6

--- src/layout/penguji/aturan/Stage6.tsx
+++ src/layout/penguji/aturan/Stage6.tsx
@@ -4,12 +4,19 @@ const Stage6 = () => {
     return (
         <Aturan
             title="Stage #6"
-            jarak="7"
-            waktu="6"
-            sasaran="1"
-            tembakMaks='18'
+            jarak="15 dan 10"
+            waktu="15"
+            sasaran="2"
+            tembakMaks='12'
             link='stage6_form'
-        />
+            posisiAwal='Berdiri rileks menghadap sasaran, kedua tangan disamping.'
+        >
+            <li>Cabut senjata dan tembak 2 kali posisi “A” (15 meter) pada sasaran Pertama, kemudian ganti magasin sambil bergerak keposisi “B” (10 meter) dan tembakan
+                dua kali pada sasaran berikutnya.</li>
+            <li>Lakukan sebanyak 3 kali.</li>
+            <li>Tembakan yang tidak mengenai sasaran (MISS) berarti gagal dalam seri tersebut.</li>
+            <li>Pelanggaran keamanan/safety akan <b>didiskualifikasi</b> dan dinyatakan <b>TIDAK LULUS</b> dari Ujian Sertifikasi dan tidak dapat mengulang pada ujian kali ini.</li>
+        </Aturan>
     )
 }
 
