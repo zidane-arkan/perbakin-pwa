35c8b4ca12f5fa60db1901757fd0301f4ea65e07
zidane-arkan <zidark897@Gmail.com>
2023-07-14 16:33:27 +0700

    Update aturan for Stage 5

--- src/layout/penguji/aturan/Stage5.tsx
+++ src/layout/penguji/aturan/Stage5.tsx
@@ -9,7 +9,13 @@ const Stage5 = () => {
             sasaran="1"
             tembakMaks='18'
             link='stage5_form'
-        />
+            posisiAwal='Berdiri membelakangi sasaran, kedua pergelangan tangan diangkat diatas bahu.'
+        >
+            <li>Cabut senjata dan tembak 2 kali pada setiap sasaran, kemudian ganti magasin dan tembak 2 kali pada sasaran yang sama.</li>
+            <li>Lakukan sebanyak 2 kali.</li>
+            <li>Tembakan yang tidak mengenai sasaran (MISS) berarti gagal dalam seri tersebut.</li>
+            <li>Pelanggaran keamanan/safety akan <b>didiskualifikasi</b> dan dinyatakan <b>TIDAK LULUS</b> dari Ujian Sertifikasi dan tidak dapat mengulang pada ujian kali ini.</li>
+        </Aturan>
     )
 }
 
