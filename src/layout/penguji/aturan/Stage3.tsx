be241b27e0a53a0fbd25e7f1034e0cc74d70c7a2
zidane-arkan <zidark897@Gmail.com>
2023-07-14 16:26:26 +0700

    Update aturan for Stage 3

--- src/layout/penguji/aturan/Stage3.tsx
+++ src/layout/penguji/aturan/Stage3.tsx
@@ -9,7 +9,13 @@ const Stage3 = () => {
             sasaran="1"
             tembakMaks='18'
             link='stage3_form'
-        />
+            posisiAwal='Berdiri rileks menghadap sasaran, kedua tangan disamping.'
+        >
+            <li>Cabut senjata dan tembak 2 kali, kemudian kunci dan sarungkan.</li>
+            <li>Lakukan sebanyak 6 kali.</li>
+            <li>Tembakan yang tidak mengenai sasaran (MISS) berarti gagal dalam seri tersebut.</li>
+            <li>Pelanggaran keamanan/safety akan <b>didiskualifikasi</b> dan dinyatakan <b>TIDAK LULUS</b> dari Ujian Sertifikasi dan tidak dapat mengulang pada ujian kali ini.</li>
+        </Aturan>
     )
 }
 
