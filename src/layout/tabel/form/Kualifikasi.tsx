8292c4f4c42adf154bf26d44b108a8babad9de88
zidane-arkan <zidark897@Gmail.com>
2023-07-16 15:31:53 +0700

    Update Kualifikasi table V1.3

--- src/layout/tabel/form/Kualifikasi.tsx
+++ src/layout/tabel/form/Kualifikasi.tsx
@@ -4,11 +4,15 @@
 //   getCoreRowModel,
 //   useReactTable,
 // } from '@tanstack/react-table';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState, ChangeEvent } from 'react';
 import styled from 'styled-components';
 
 
 const Styles = styled.div`
+  input{
+    width: 2rem;
+    text-align: center;
+  }
   input[type=number]::-webkit-inner-spin-button, 
   input[type=number]::-webkit-outer-spin-button {  
     -webkit-appearance: "Always Show Up/Down Arrows";
@@ -16,12 +20,22 @@ const Styles = styled.div`
     -moz-appearance: textfield;
     appearance: textfield;
     opacity: 1;
+    text-align: center;
   }
 
   padding: 1rem 0;
   overflow : scroll;
+  @media screen and (min-width: 600px){
+    table{
+      width: 100%;
+    }
+  input{
+    width: 3rem;
+    text-align: center;
+  }
+  }
   table {
-    width: 400px;
+    width: 100%;
     text-align: center;
     border-spacing: 0;
     border-right: 2px solid #D5E4F0;
@@ -43,6 +57,7 @@ const Styles = styled.div`
       background-color : #036BB0;
     }
     th{
+      text-align: center;
       margin: 0;
       padding: 0.5rem;
       border-bottom: 2px solid #D5E4F0;
@@ -50,6 +65,7 @@ const Styles = styled.div`
       border-left: 2px solid #D5E4F0;
     }
     td {
+      text-align: center;
       margin: 0;
       padding: 0.3rem;
       border-bottom: 2px solid #D5E4F0;
@@ -71,79 +87,124 @@ const Styles = styled.div`
   }
 `
 
+interface TableData {
+  stage_0: {
+    series_1: number[];
+    series_2: number[];
+    series_3: number[];
+    series_4: number[];
+    series_5: number[];
+    checkmarks: boolean[];
+  };
+}
+
 const Percobaan1 = () => {
-  const [tableData, setTableData] = useState([
-    {
-      Seri: 1,
-      NilaiPerkenaan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      Total: 0,
-      HasilSeri: false
+  const seriesNames = ["1", "2", "3", "4", "5"];
+  const [tableData, setTableData] = useState<TableData | any>({
+    stage_0: {
+      series_1: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      series_2: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      series_3: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      series_4: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      series_5: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      checkmarks: [false, false, false, false, false],
     },
-    {
-      Seri: 2,
-      NilaiPerkenaan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      Total: 0,
-      HasilSeri: false
-    },
-    {
-      Seri: 3,
-      NilaiPerkenaan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      Total: 0,
-      HasilSeri: false
-    },
-    {
-      Seri: 4,
-      NilaiPerkenaan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      Total: 0,
-      HasilSeri: false
-    },
-    {
-      Seri: 5,
-      NilaiPerkenaan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      Total: 0,
-      HasilSeri: false
-    }
-  ]);
+  });
 
   const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    seriIndex: number,
-    nilaiIndex: number
+    e: ChangeEvent<HTMLInputElement>,
+    stageKey: string | any,
+    seriesKey: string,
+    index: number
   ) => {
     const { value } = e.target;
-    const updatedData = [...tableData];
-    updatedData[seriIndex].NilaiPerkenaan[nilaiIndex] = parseInt(value, 10);
-
-    // Hitung hasil penjumlahan dari kolom nilai perkenaan
-    const total = updatedData[seriIndex].NilaiPerkenaan.reduce(
-      (acc, cur) => acc + cur,
-      0
-    );
-    updatedData[seriIndex].Total = total;
-
-    setTableData(updatedData);
+    setTableData((prevData: any) => {
+      const updatedData: any = { ...prevData };
+      updatedData[stageKey][seriesKey][index] = parseInt(value, 10);
+      updatedData[stageKey][seriesKey][11] = calculateTotal(
+        updatedData[stageKey][seriesKey].slice(0, 11) as number[]
+      );
+      return updatedData;
+    });
   };
 
   const handleCheckbox = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    seriIndex: number
+    e: ChangeEvent<HTMLInputElement>,
+    stageKey: string,
+    index: number
   ) => {
     const { checked } = e.target;
-    const updatedData = [...tableData];
-    updatedData[seriIndex].HasilSeri = checked;
-    setTableData(updatedData);
+    setTableData((prevData: any) => {
+      const updatedData: any = { ...prevData };
+      updatedData[stageKey].checkmarks[index] = checked;
+      return updatedData;
+    });
     console.log(tableData);
   };
 
+  const calculateTotal = (seriesValues: number[]) => {
+    return seriesValues
+      .slice(0, 11)
+      .reduce((acc, cur, index) => acc + cur * index, 0);
+  };
+
+  const renderSeries = (stageKey: string | any) => {
+    const stageData = tableData[stageKey];
+    return Object.entries(stageData)
+      .filter(([key]) => key !== "checkmarks")
+      .map(([seriesKey, seriesValues], index) => {
+        const total = calculateTotal(seriesValues as number[]);
+        const isDisabled = total < 70;
+
+        return (
+          <React.Fragment key={seriesKey}>
+            <tr>
+              <td rowSpan={2}>{seriesNames[index]}</td>
+              {(seriesValues as number[]).slice(0, 11).map((nilai: number, nilaiIndex: number) => (
+                <td key={nilaiIndex}>
+                  <input
+                    type="number"
+                    min={0}
+                    max={10}
+                    value={nilai}
+                    onChange={(e) =>
+                      handleInputChange(e, stageKey, seriesKey, nilaiIndex)
+                    }
+                  />
+                </td>
+              ))}
+              <td rowSpan={2}>{total}</td>
+              <td rowSpan={2}>
+                <input
+                  type="checkbox"
+                  id={`${stageKey}-seri-${index}`}
+                  name={`${stageKey}-seri-${index}`}
+                  value="benar"
+                  checked={stageData.checkmarks[index]}
+                  disabled={isDisabled}
+                  onChange={(e) => handleCheckbox(e, stageKey, index)}
+                />
+              </td>
+            </tr>
+            <tr>
+              {(seriesValues as number[]).slice(0, 11).map((nilai: number, nilaiIndex: number) => (
+                <td key={nilaiIndex}>{nilai}</td>
+              ))}
+            </tr>
+          </React.Fragment>
+        );
+      });
+  };
+
   return (
-    <div className="App">
+    <Styles>
       <table>
         <thead>
           <tr>
             <th colSpan={1}>Seri</th>
             <th colSpan={11}>Nilai Perkenaan</th>
-            <th colSpan={1}>Total</th>
-            <th colSpan={1}>Hasil Seri</th>
+            <th rowSpan={2}>Jumlah </th>
+            <th rowSpan={2}>Hasil </th>
           </tr>
           <tr>
             <th>No Seri</th>
@@ -158,58 +219,19 @@ const Percobaan1 = () => {
             <th>8</th>
             <th>9</th>
             <th>10</th>
-            <th>Jumlah</th>
-            <th>Hasil</th>
           </tr>
         </thead>
-        <tbody>
-          {tableData.map((row, index) => (
-            <React.Fragment key={index}>
-              <tr>
-                <td rowSpan={2}>{row.Seri}</td>
-                {row.NilaiPerkenaan.map((nilai, nilaiIndex) => (
-                  <td key={nilaiIndex}>
-                    <input
-                      className='w-12 h-8 text-black text-center opacity-100'
-                      type="number"
-                      min={0}
-                      max={10}
-                      value={nilai}
-                      onChange={(e) => handleInputChange(e, index, nilaiIndex)}
-                    />
-                  </td>
-                ))}
-                <td rowSpan={2}>{row.Total}</td>
-                <td rowSpan={2}>
-                  <input
-                    type="checkbox"
-                    id={`seri-${index}`}
-                    name={`seri-${index}`}
-                    value="benar"
-                    checked={row.HasilSeri}
-                    onChange={(e) => handleCheckbox(e, index)}
-                  />
-                </td>
-              </tr>
-              <tr>
-                {row.NilaiPerkenaan.map((nilai, nilaiIndex) => (
-                  <td key={nilaiIndex}>{nilai}</td>
-                ))}
-              </tr>
-            </React.Fragment>
-          ))}
-        </tbody>
+        <tbody>{renderSeries("stage_0")}</tbody>
       </table>
-    </div>
+    </Styles>
   );
 };
 
 const Kualifikasi = () => {
-
   return (
-    <Styles>
+    <>
       <Percobaan1 />
-    </Styles>
+    </>
   )
 }
 
@@ -562,3 +584,159 @@ export default Kualifikasi
 //     </Styles>
 //   )
 // }
+
+
+// Percobaan 1
+// const Percobaan1 = () => {
+//   const [tableData, setTableData] = useState([
+//     {
+//       Seri: 1,
+//       NilaiPerkenaan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       Total: 0,
+//       HasilSeri: false
+//     },
+//     {
+//       Seri: 2,
+//       NilaiPerkenaan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       Total: 0,
+//       HasilSeri: false
+//     },
+//     {
+//       Seri: 3,
+//       NilaiPerkenaan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       Total: 0,
+//       HasilSeri: false
+//     },
+//     {
+//       Seri: 4,
+//       NilaiPerkenaan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       Total: 0,
+//       HasilSeri: false
+//     },
+//     {
+//       Seri: 5,
+//       NilaiPerkenaan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       Total: 0,
+//       HasilSeri: false
+//     }
+//   ]);
+
+//   const handleInputChange = (
+//     e: React.ChangeEvent<HTMLInputElement>,
+//     seriIndex: number,
+//     nilaiIndex: number
+//   ) => {
+//     const { value } = e.target;
+//     const updatedData = [...tableData];
+//     updatedData[seriIndex].NilaiPerkenaan[nilaiIndex] = parseInt(value, 10);
+
+//     // Hitung hasil penjumlahan dari kolom nilai perkenaan
+//     const total = updatedData[seriIndex].NilaiPerkenaan.reduce(
+//       (acc, cur) => acc + cur,
+//       0
+//     );
+//     updatedData[seriIndex].Total = total;
+
+//     setTableData(updatedData);
+//   };
+
+//   const handleCheckbox = (
+//     e: React.ChangeEvent<HTMLInputElement>,
+//     seriIndex: number
+//   ) => {
+//     const { checked } = e.target;
+//     const updatedData = [...tableData];
+//     updatedData[seriIndex].HasilSeri = checked;
+
+//     // Periksa total skor seri sekarang
+//     const totalSeri = updatedData[seriIndex].Total;
+
+//     // Periksa apakah total skor mencapai 70 atau lebih
+//     if (totalSeri >= 70) {
+//       setTableData(updatedData);
+//     } else {
+//       // Jika total skor belum mencapai 70, set HasilSeri ke false
+//       updatedData[seriIndex].HasilSeri = false;
+//       setTableData(updatedData);
+//       console.log('Total skor harus mencapai 70 atau lebih untuk menandai serinya.');
+//     }
+//   };
+//   // const handleCheckbox = (
+//   //   e: React.ChangeEvent<HTMLInputElement>,
+//   //   seriIndex: number
+//   // ) => {
+//   //   const { checked } = e.target;
+//   //   const updatedData = [...tableData];
+//   //   updatedData[seriIndex].HasilSeri = checked;
+//   //   setTableData(updatedData);
+//   //   console.log(tableData);
+//   // };
+
+//   return (
+//     <div className="App">
+//       <table>
+//         <thead>
+//           <tr>
+//             <th colSpan={1}>Seri</th>
+//             <th colSpan={11}>Nilai Perkenaan</th>
+//             <th colSpan={1}>Total</th>
+//             <th colSpan={1}>Hasil Seri</th>
+//           </tr>
+//           <tr>
+//             <th>No Seri</th>
+//             <th>0</th>
+//             <th>1</th>
+//             <th>2</th>
+//             <th>3</th>
+//             <th>4</th>
+//             <th>5</th>
+//             <th>6</th>
+//             <th>7</th>
+//             <th>8</th>
+//             <th>9</th>
+//             <th>10</th>
+//             <th>Jumlah</th>
+//             <th>Hasil</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {tableData.map((row, index) => (
+//             <React.Fragment key={index}>
+//               <tr>
+//                 <td rowSpan={2}>{row.Seri}</td>
+//                 {row.NilaiPerkenaan.map((nilai, nilaiIndex) => (
+//                   <td key={nilaiIndex}>
+//                     <input
+//                       className='w-12 h-8 text-black text-center opacity-100'
+//                       type="number"
+//                       min={0}
+//                       max={10}
+//                       value={nilai}
+//                       onChange={(e) => handleInputChange(e, index, nilaiIndex)}
+//                     />
+//                   </td>
+//                 ))}
+//                 <td rowSpan={2}>{row.Total}</td>
+//                 <td rowSpan={2}>
+//                   <input
+//                     type="checkbox"
+//                     id={`seri-${index}`}
+//                     name={`seri-${index}`}
+//                     value="benar"
+//                     checked={row.HasilSeri}
+//                     onChange={(e) => handleCheckbox(e, index)}
+//                   />
+//                 </td>
+//               </tr>
+//               <tr>
+//                 {row.NilaiPerkenaan.map((nilai, nilaiIndex) => (
+//                   <td key={nilaiIndex}>{nilai}</td>
+//                 ))}
+//               </tr>
+//             </React.Fragment>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
