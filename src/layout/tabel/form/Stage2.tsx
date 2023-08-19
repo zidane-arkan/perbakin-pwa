1ff55d495e17d84803c9099c235801c759b190ba
zidane-arkan <zidark897@Gmail.com>
2023-08-19 18:14:11 +0700

    FIx loading in Stage 1 and 2

--- src/layout/tabel/form/Stage2.tsx
+++ src/layout/tabel/form/Stage2.tsx
@@ -104,7 +104,7 @@ const Percobaan1: React.FC<Percobaan1Props> = ({ shooterid }: any) => {
     // const [isTimeoutCleared, setIsTimeoutCleared] = useState(true);
     // STATUS INPUT
     const [status, setStatus] = useState<number>(0);
-
+    const [loading, setLoading] = useState(true);
     // CONVERT DATA FROM API TO TABLE DATA
     // Fetch data from API and update tableData
     useEffect(() => {
@@ -145,11 +145,13 @@ const Percobaan1: React.FC<Percobaan1Props> = ({ shooterid }: any) => {
                 }).filter(Boolean) as TableDataItem[];
 
                 setTableData(updatedTableData);
+                setLoading(false);
             } catch (error) {
                 console.error(error);
+                setLoading(false);
             }
         };
-
+        setLoading(true);
         // Cleanup function to reset tableData before fetching new data
         setTableData([]); // Clear existing tableData
         fetchTry1Data();
@@ -263,7 +265,6 @@ const Percobaan1: React.FC<Percobaan1Props> = ({ shooterid }: any) => {
         }
     };
 
-
     // HANDLE DATA REACT
     const handleInputChange = <K extends keyof TableDataItem>(e: React.ChangeEvent<HTMLInputElement>, id: number, field: K) => {
         const { value } = e.target;
@@ -364,6 +365,34 @@ const Percobaan1: React.FC<Percobaan1Props> = ({ shooterid }: any) => {
         updateCheckmarksBE(updatedCheckmarks);
     };
 
+    if (loading) {
+        return (
+            <div className='flex flex-col w-full p-4 gap-4 border border-solid border-blue-500 rounded-xl mt-4'>
+                <h2 className="mb-2 text-lg font-semibold text-gray-900">Mengambil data tabel:</h2>
+                <ul className="max-w-md space-y-2 text-gray-500 list-inside dark:text-gray-400">
+                    <li className="flex items-center">
+                        <svg className="w-4 h-4 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        Menghubungkan dengan database
+                    </li>
+                    <li className="flex items-center">
+                        <svg className="w-4 h-4 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                        Mengubah Bentuk data
+                    </li>
+                    <li className="flex items-center">
+                        <div role="status">
+                            <svg aria-hidden="true" className="w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" /><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" /></svg>
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                        Memasukkan Data kedalam Tabel
+                    </li>
+                </ul>
+            </div>
+        )
+    }
     return (
         <section>
             <table>
@@ -888,7 +917,6 @@ const Stage2 = () => {
     const [isLoading, setIsLoading] = useState(true);
     // const [stage2Data, setStage1Data] = useState();
     const [try2Status, setTry2Status] = useState(false);
-
     const fetchTry1Data = async () => {
         try {
             const response = await api.get(`/scorer/shooter/${shooterid}/result/stage2`);
